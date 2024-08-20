28ab40c6bcb94299520cff9c5d8c578f88ed710e
jaepil <jaepill94@gmail.com>
2024-08-20 11:32:36 +0900

    first commit

---
+++ src/main/java/project/forwork/ForworkApplication.java
@@ -0,0 +1,13 @@
+package project.forwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/forwork/ForworkApplicationTests.java
@@ -0,0 +1,13 @@
+package project.forwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
