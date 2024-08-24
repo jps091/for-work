d87dfbaa01ad52c9a45d0e190ee32f8c7ed63e2e
jaepil <jaepill94@gmail.com>
2024-08-24 19:31:21 +0900

    feat: WebConfig 구현완료
    - 인터셉터를 활용해서 화이트리스트 구성완료.
    Related to: #9

--- src/main/java/project/forwork/api/common/config/web/WebConfig.java
+++ src/main/java/project/forwork/api/common/config/web/WebConfig.java
@@ -2,6 +2,7 @@ package project.forwork.api.common.config.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import project.forwork.api.interceptor.AuthorizationInterceptor;
