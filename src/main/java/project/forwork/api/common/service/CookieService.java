5894b893c699f95a8b98c37bbed900b2316c05ad
jaepil <jaepill94@gmail.com>
2024-08-25 18:48:33 +0900

    fix: CookieService Stream
    - 중첩 반목문과 조건문을 Stream, Optional을 활욜해서 리팩토링 작업 완료.
    Related to: #12

--- src/main/java/project/forwork/api/common/service/CookieService.java
+++ src/main/java/project/forwork/api/common/service/CookieService.java
@@ -11,6 +11,9 @@ import project.forwork.api.domain.token.model.TokenResponse;
 import project.forwork.api.domain.token.service.TokenService;
 import project.forwork.api.domain.user.model.User;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CookieService {
@@ -44,6 +47,19 @@ public class CookieService {
     }
 
     private void deleteCookie(HttpServletRequest request, HttpServletResponse response, String tokenKey) {
+        Optional.ofNullable(request.getCookies())
+                .stream()
+                .flatMap(Arrays::stream)  // Cookie[] -> Stream<Cookie>
+                .filter(cookie -> tokenKey.equals(cookie.getName()))  // tokenKey와 이름이 일치하는 쿠키 필터링
+                .forEach(cookie -> {  // 각 쿠키에 대해 아래 작업 수행
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    cookie.setValue("");
+                    response.addCookie(cookie);
+                });
+    }
+
+    private void deleteCookie2(HttpServletRequest request, HttpServletResponse response, String tokenKey) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
@@ -58,6 +74,16 @@ public class CookieService {
     }
 
     public String extractTokenFromCookies(HttpServletRequest request, String tokenKey) {
+        return Optional.ofNullable(request.getCookies())
+                .stream()
+                .flatMap(Arrays::stream)  // Cookie[] -> Stream<Cookie>
+                .filter(cookie -> tokenKey.equals(cookie.getName()))  // tokenKey와 이름이 일치하는 쿠키 필터링
+                .map(Cookie::getValue)  // 쿠키의 값을 가져옴
+                .findFirst()  // 첫 번째 값을 Optional로 반환
+                .orElseThrow(() -> new ApiException(TokenErrorCode.INVALID_TOKEN));  // 값이 없으면 예외 발생
+    }
+
+    public String extractTokenFromCookies2(HttpServletRequest request, String tokenKey) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
             for (Cookie cookie : cookies) {
