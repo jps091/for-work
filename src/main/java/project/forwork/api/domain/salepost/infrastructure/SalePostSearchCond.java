57a82d01ca4cecfdd53a0a40f864fdd4b5e0464d
jaepil <jaepill94@gmail.com>
2024-09-20 16:29:14 +0900

    refactor: 판매글 검색 조건 제목제거
    - 인덱스를 걸어야 할 상황을 생각하여 꼭 필요하지 않은 제목 조건 제거
    Related to: #54

--- src/main/java/project/forwork/api/domain/salepost/infrastructure/SalePostSearchCond.java
+++ src/main/java/project/forwork/api/domain/salepost/infrastructure/SalePostSearchCond.java
@@ -8,7 +8,6 @@ import java.math.BigDecimal;
 
 @Data // TODO 검색 조건 제목 제거 해야함
 public class SalePostSearchCond {
-    private String title;
     private BigDecimal minPrice;
     private BigDecimal maxPrice;
     private FieldType field;
