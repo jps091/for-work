080622e4aa456d24781ed2d3866f9c62202825aa
jaepil <jaepill94@gmail.com>
2024-10-21 09:45:20 +0900

    feat: Valid 관련 예외처리기 작성
    - 기존에는 HandlerMethodValidationException 가 발생하면 500 서버에러로 반환 했지만 전용 예외 처리기를 작성 해서 400 상태코드 반환과 사용자에게 에러 원인 알려주는 방향으로 기능 추가 하였습니다.
    Resolves: #128

--- src/main/java/project/forwork/api/common/controller/MethodArgumentNotValidExceptionHandler.java
+++ src/main/java/project/forwork/api/common/controller/MethodArgumentNotValidExceptionHandler.java
@@ -1,54 +1,37 @@
 package project.forwork.api.common.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import project.forwork.api.common.api.Api;
-import project.forwork.api.common.error.ErrorCodeIfs;
-import project.forwork.api.common.exception.ApiException;
+import project.forwork.api.common.error.ErrorCode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 @Order(value = Integer.MIN_VALUE + 1)
 public class MethodArgumentNotValidExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Api<Object>> handleValidationExceptions(MethodArgumentNotValidException ex){
-        log.error("MethodArgumentNotValidException ex", ex);
-        StringBuilder errorMessages = new StringBuilder();
+    @ExceptionHandler({HandlerMethodValidationException.class})
+    public ResponseEntity<Api<Object>> handleValidationExceptions(HandlerMethodValidationException ex){
+        log.error("MethodArgumentNotValidExceptionHandler", ex);
 
-        /*** TODO
-         *     {
-         *         "objectName": "resumeRegisterRequest",
-         *         "field": "price",
-         *         "rejectedValue": 100100,
-         *         "defaultMessage": "최대 판매 가격은 100,000원 이하이어야 합니다."
-         *     }
-         */
+        String message = ex.getAllValidationResults().stream()
+                .map(ParameterValidationResult::getResolvableErrors)
+                .flatMap(List::stream)
+                .map(MessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
 
-        ex.getBindingResult().getAllErrors().forEach(error -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errorMessages.append(fieldName).append(": ").append(errorMessage).append("; ");
-        });
-
-        Api<Object> apiErrorResponse = Api.ERROR(
-                400,
-                "잘못된 요청",
-                errorMessages.toString().trim()
-        );
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiErrorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Api.ERROR(ErrorCode.BAD_REQUEST, message));
     }
 }
 
