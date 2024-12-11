d489ae0273868abea393aafebdcfbf3c5bd46439
jaepil <jaepill94@gmail.com>
2024-12-11 19:01:31 +0900

    refactor: 결제 예외 처리 수정
    - Connect, Socket 타임아웃 시간 설정
    - PaymentsService 발생한 예외 CheckoutService에서 처리
    Resolves: #200

--- src/main/java/project/forwork/api/common/error/TransactionErrorCode.java
+++ src/main/java/project/forwork/api/common/error/TransactionErrorCode.java
@@ -9,7 +9,8 @@ public enum TransactionErrorCode implements ErrorCodeIfs {
 
     TX_AlREADY_PAYMENT(400, 10401, "이미 결제된 거래 입니다."),
     TX_NOT_FOUND(404, 10402, "결제 내역이 존재 하지 않습니다"),
-    SERVER_ERROR(500, 10403, "시스템 오류입니다. 잠시후 다시 시도 해주세요."),
+    TX_SERVER_ERROR(500, 10403, "시스템 오류입니다. 잠시후 다시 시도 해주세요."),
+    TX_CLIENT_ERROR(400, 10404, "결제를 실패 하였습니다. 결제 관련 정보를 확인후 다시 시도 해주세요."),
     ;
 
     private final Integer httpStatusCode;
